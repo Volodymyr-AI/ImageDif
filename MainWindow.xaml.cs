0030e438da805a76c07aa5795a424a9e1749b11b
Volodymyr-AI <alternativeqoob@gmail.com>
2023-11-23 01:35:18 +0200

    Decidedd to stop on 2 images

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace ImageDif
             var values = new ChartValues<double> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
             chart.Series[0].Values = values;
             chart.AxisY[0].MinValue = 0;
+            chart.AxisY[0].MaxValue = 30;
         }
 
         private void Border_Drop(object sender, DragEventArgs e)
@@ -71,16 +72,23 @@ namespace ImageDif
                   }
               }
         }
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private void Border_Drop2(object sender, DragEventArgs e)
         {
-            // Get the image from the DroppedImage control
-            BitmapImage image = DroppedImage.Source as BitmapImage;
-
-            // Check if an image is loaded
-            if (image != null)
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string filePath = files[0];
+
+                // Load the image
+                BitmapImage image = new BitmapImage(new Uri(filePath));
+
+                // Resize the image
+                DroppedImage2.Width = 150;
+                DroppedImage2.Height = 200;
+                DroppedImage2.Source = image;
+
                 // Save the image to the DownloadableImages folder
-                string savePath = Path.Combine("D:\\University\\Img\\ImageDif\\DownloadableImages", "saved_image.jpg");
+                string savePath = Path.Combine("D:\\University\\Img\\ImageDif\\DownloadableImages", Path.GetFileName(filePath));
                 string directoryPath = Path.GetDirectoryName(savePath);
                 if (!Directory.Exists(directoryPath))
                 {
@@ -102,7 +110,43 @@ namespace ImageDif
                 }
             }
         }
-        private async void ResetButton_Click(object sender, RoutedEventArgs e)
+        private void SaveImage(Image image, string savePath)
+        {
+            // Get the image from the DroppedImage control
+            BitmapImage bitmapImage = image.Source as BitmapImage;
+
+            // Check if an image is loaded
+            if (bitmapImage != null)
+            {
+                // Save the image to the DownloadableImages folder
+                string directoryPath = Path.GetDirectoryName(savePath);
+                if (!Directory.Exists(directoryPath))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(directoryPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error occurred while creating the directory: " + ex.Message);
+                        return;
+                    }
+                }
+                using (FileStream stream = new FileStream(savePath, FileMode.Create))
+                {
+                    JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                    encoder.Save(stream);
+                }
+            }
+        }
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Call the SaveImage method for each image
+            SaveImage(DroppedImage, "D:\\University\\Img\\ImageDif\\DownloadableImages\\saved_image1.jpg");
+            SaveImage(DroppedImage2, "D:\\University\\Img\\ImageDif\\DownloadableImages\\saved_image2.jpg");
+        }
+        private void DeleteImage(Image image)
         {
             // Get the directory path
             string directoryPath = "D:\\University\\Img\\ImageDif\\DownloadableImages";
@@ -114,8 +158,15 @@ namespace ImageDif
                 Directory.EnumerateFiles(directoryPath).ToList().ForEach(File.Delete);
 
                 // Clear the DroppedImage control
-                DroppedImage.Source = null;
+                image.Source = null;
             }
+        }
+
+        private async void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Call the DeleteImage method for each image
+            DeleteImage(DroppedImage);
+            DeleteImage(DroppedImage2);
         }
         private void OpenFolder(object sender, RoutedEventArgs e)
         {
@@ -124,7 +175,7 @@ namespace ImageDif
     }
 }
 
-// checking if saved img exists
+// checking if saved imge exists
 //string imagePath = @"D:\University\Img\ImageDif\DownloadableImages\saved_image.jpg";
 //if (File.Exists(imagePath))
 //{
